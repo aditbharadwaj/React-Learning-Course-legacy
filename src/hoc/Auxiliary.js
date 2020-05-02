d1e498a78f3fa292a89a9f397f28fdbe9774f958
adit <tidabharadwaj04@gmail.com>
2020-05-02 18:02:56 +0530

    this commit includes >> project restructure and initial balnk design

--- src/hoc/Auxiliary.js
+++
@@ -1,7 +0,0 @@
-/* this is a high level component which 
-we use to wrap a react element without 
-returning multiple elements
- */
-const auxiliary = props => props.children;
-
-export default auxiliary;
