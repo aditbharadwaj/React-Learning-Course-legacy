65df429673035adf545c9fb0d41f5e7ab40360ff
adit <tidabharadwaj04@gmail.com>
2020-05-06 19:58:58 +0530

    this commit includes >> creating navigation bar for mobile and desktop, creating logo component , creating side drawer menu component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A Delicious burger with the following ingredients</p>
       <ul>{ingredientSummary}</ul>
+      <p>Total Price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
       <p>Continue to Checkout ?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
         CANCEL
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Layout.css";
 import Auxillary from "../../hoc/Auxillary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Auxillary>
-    <div>Toolbar,SideDrawer , Back drop</div>
-    <main className={classes.Content}>
-    {props.children}
-    </main>
-  </Auxillary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  //setting staates when it depends on the old state
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
 
-export default layout;
+  render() {
+    return (
+      <Auxillary>
+        <Toolbar drawerToggledClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxillary>
+    );
+  }
+}
+
+export default Layout;
